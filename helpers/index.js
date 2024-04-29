d62d4943be3c710f7ec6e612d1c1a1139eee63ef
Vasyl Dutchak <code.genius.web.dev@gmail.com>
2024-04-29 16:55:48 +0200

    add reexport for helpers

--- helpers/index.js
+++ helpers/index.js
@@ -2,7 +2,8 @@ const HttpError=require('./HttpError')
 const ctrlWrapper=require('./ctrlWrapper')
 const handleMongooseError=require('./handleMongooseError')
 const sendEmail=require('./sendEmail')
+const USER_ROLES=require('./userRoles')
 
 module.exports={
-    HttpError,ctrlWrapper,handleMongooseError,sendEmail
+    HttpError,ctrlWrapper,handleMongooseError,sendEmail,USER_ROLES,
 }
