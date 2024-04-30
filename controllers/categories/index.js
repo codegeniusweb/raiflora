c534beb2c22e61d12828574acbdda57d28a6a675
Vasyl Dutchak <code.genius.web.dev@gmail.com>
2024-04-30 19:49:09 +0200

    make reexport for controllers

---
+++ controllers/categories/index.js
@@ -0,0 +1,7 @@
+const addCategory=require('./addCategory')
+const getAllCategories=require('./getAllCategories')
+const getByPublicId=require('./getByPublicId')
+const deleteByPublicId=require('./deleteByPublicId')
+const updateByPublicId=require('./updateByPublicId')
+
+module.exports={addCategory,getAllCategories,getByPublicId,deleteByPublicId,updateByPublicId}
