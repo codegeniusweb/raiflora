e3f59dbcacd7f435b4bd6d2275a3d20206d6b0d7
Vasyl Dutchak <code.genius.web.dev@gmail.com>
2024-04-30 19:43:17 +0200

    change logic in method

--- middlewares/checkUserRole.js
+++ middlewares/checkUserRole.js
@@ -1,10 +1,13 @@
 const {HttpError, USER_ROLES} = require("../helpers");
 
 
-const checkUserRole =  (req, res, next) => {
-    const {_id,role}=req.user
+const checkUserRole = (permissionRole)=> (req, res, next) => {
+    /*const {_id,role}=req.user
     const {id}=req.params
     if(_id.toString()===id || role===USER_ROLES.ADMIN) return next()
+    next(HttpError(403, 'User has not permission '))*/
+    const {role}=req.user
+    if(permissionRole===role) return next()
     next(HttpError(403, 'User has not permission '))
 }
 
